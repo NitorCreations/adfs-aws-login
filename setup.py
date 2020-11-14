c80209c5e70b7212e33e185040f706fd7f3017c8
Pasi Niemi <pasi.niemi@nitor.com>
2020-11-14 19:42:28 +0200

    Fix bug with ADFS_DEFAULT_PASSWORD

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.md') as f:
     long_description = f.read()
 
 setup(name='adfs-aws-login',
-      version='0.2.0',
+      version='0.2.1',
       description='CLI login to AWS using ADFS',
       long_description=long_description,
       long_description_content_type='text/markdown',
